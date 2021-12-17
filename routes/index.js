27b6625adb0eef54cabfc404b48de7f2a6a85b81
Hung Duong Quang <qhungg289@protonmail.com>
2021-12-17 20:35:52 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date(),
+	},
+	{
+		text: "Hello World!",
+		user: "Charles",
+		added: new Date(),
+	},
+];
+
+router.get("/", function (req, res) {
+	res.render("index", { title: "Mini Message", messages: messages });
+});
+
+router.get("/new", function (req, res) {
+	res.render("form", { title: "Mini Message" });
+});
+
+router.post("/new", function (req, res) {
+	messages.push({
+		text: req.body.message,
+		user: req.body.author,
+		added: new Date(),
+	});
+	res.redirect("/");
 });
 
 module.exports = router;
